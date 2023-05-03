6511da900d8621ea71b325254f2f3e47381d2a6c
Stefanie David <127105234+stefaniegdavid@users.noreply.github.com>
2023-05-02 23:16:19 -0700

    Update file.py

--- file.py
+++ file.py
@@ -17,11 +17,13 @@ for i in range(1,101):
     else:
         print(i)
 
+        
 # 4. Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
 sum = 0
 for i in range(1, 500001, 2):
     sum += i
 print(sum)
+
 
 # 5. Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
 for i in range(2018, 0, -4):
