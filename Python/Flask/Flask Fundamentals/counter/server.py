a9cb6d889a280d9372c5f1c0360583b7c8df22a0
Stefanie David <127105234+stefaniegdavid@users.noreply.github.com>
2023-05-16 21:54:20 -0700

    Update server.py

--- Python/Flask/Flask Fundamentals/counter/server.py
+++ Python/Flask/Flask Fundamentals/counter/server.py
@@ -4,29 +4,36 @@ app = Flask(__name__)
 app.secret_key = 'Gucci'
 
 @app.route('/')
-def index():
-    if "add_num" in session:
-        session['add_num'] =+ 1
+def home():
+    if "page_count" in session:
+        session["page_count"] += 1
     else:
-        session['add_num'] = 0
-        return render_template("index.html")
-    
+        session["page_count"]=1
+    if "count" not in session:
+        session["count"]=0
+    return render_template("index.html")
+
 @app.route('/count', methods=['POST'])
-def click():
-    session['add_num'] =+ int(request.form['add'])
+def count():
+    if "count" in session:
+        session["count"] += int(request.form["count"])
+    else:
+        session["count"]=int(request.form["count"])
+    return redirect('/')
+
+@app.route('/count_2', methods=['POST'])
+def count_2():
+    if "count" in session:
+        session["count"] += 2
+    else:
+        session["count"]=2
     return redirect('/')
 
 @app.route('/reset', methods=['POST'])
 def destroy_session():
     session.clear()
-    session["add_num"] = 0
+    session["count"]=0
     return redirect('/')
-
-@app.route('/plus_two', methods=['POST'])
-def plus_two():
-    session['add_num'] += 1
-    return redirect('/')
-
 
 
 if __name__ == "__main__":
